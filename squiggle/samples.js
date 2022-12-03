8379554ca82e363cdfce4b3b695f929d23fd106b
NunoSempere <nuno.sempere@protonmail.com>
2022-12-03 13:14:08 +0000

    feat: rejiggle default number of samples.

--- squiggle/samples.js
+++ squiggle/samples.js
@@ -5,9 +5,22 @@ p_a = 0.8
 p_b = 0.5
 p_c = p_a * p_b
 
-result = mx([0, 1, 1 to 3, 2 to 10], [(1 - p_c), p_c/2, p_c/4, p_c/4 ])
+dist_0 = 0
+dist_1 = 1
+dist_some = SampleSet.fromDist(1 to 3)
+dist_many = SampleSet.fromDist(2 to 10)
+
+dists = [dist_0, dist_1, dist_some, dist_many]
+weights = [(1 - p_c), p_c/2, p_c/4, p_c/4 ]
+
+result = mixture(dists, weights)
 mean(result)
+
 `
 
-let output = run(squiggle_code)
+let output = run(squiggle_code, {
+  defaultXYPointLength: 1000000,
+  defaultSampleCount: 1000000,
+  sparklineLength: 20,
+})
 console.log(output.result.value)
