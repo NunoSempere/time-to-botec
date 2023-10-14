e635cb665358034b7a4b5041ab470ef7aff817fe
NunoSempere <nuno.sempere@protonmail.com>
2023-10-14 20:12:42 +0100

    start populating samplers

--- ocaml/samples.ml
+++ ocaml/samples.ml
@@ -1,5 +1,6 @@
 (* Constants *)
 let pi = acos (-1.)
+let normal_95_ci_length = 1.6448536269514722
 
 (* Basic samplers *)
 let sampleZeroToOne () : float = Random.float 1.0
@@ -8,6 +9,14 @@ let sampleStandardNormal (): float =
   let u2 = sampleZeroToOne () in 
   let z = sqrt(-2.0 *. log(u1)) *. sin(2.0 *. pi *. u2) in 
   z
+let sampleNormal mean std = mean +. std *. (sampleStandardNormal ())
+let sampleLognormal logmean logstd = exp(sampleNormal logmean logstd)
+let sampleTo low high = 
+  let loglow = log(low) in
+  let loghigh = log(high) in
+  let logmean = (loglow +. loghigh) /. 2.0 in
+  let logstd = (loghigh -. loglow) /. (2.0 -. normal_95_ci_length ) in
+  sampleLognormal logmean logstd
 
 let () =
   Random.init 1;
