f8cc46ad151b2af63b66729dd17cf055bd9809f3
Nuño Sempere <nuno.semperelh@protonmail.com>
2024-04-05 23:22:20 +0000

    use a simpler & faster prng

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,17 @@
-use rand::thread_rng;
+use rand_core::SeedableRng;
+use rand_pcg::Pcg64Mcg;
 use rand_distr::{Normal, Distribution};
+
+// use rand::thread_rng;
+// use rand::prelude::*
 
 fn main() {
     println!("Hello, world!");
 
+    let mut rng = Pcg64Mcg::seed_from_u64(1);
     // mean 2, standard deviation 3
     let normal = Normal::new(2.0, 3.0).unwrap();
-    let v = normal.sample(&mut thread_rng());
+    let v = normal.sample(&mut rng);
     println!("{} is from a N(2, 9) distribution", v)
 }
 
