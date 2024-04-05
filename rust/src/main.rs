25bb2c3769b56a6249dd06f24e7af2ec6d3b891b
Nuño Sempere <nuno.semperelh@protonmail.com>
2024-04-05 23:06:19 +0000

    sample normal in Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,13 @@
+use rand::thread_rng;
+use rand_distr::{Normal, Distribution};
+
 fn main() {
     println!("Hello, world!");
+
+    // mean 2, standard deviation 3
+    let normal = Normal::new(2.0, 3.0).unwrap();
+    let v = normal.sample(&mut thread_rng());
+    println!("{} is from a N(2, 9) distribution", v)
 }
+
+
