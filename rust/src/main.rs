734bb34af3e64a2bbfe77162ec8ac28fb792c32b
NunoSempere <nuno.semperelh@protonmail.com>
2024-04-06 13:40:39 -0400

    have two layers of mutability

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,7 +5,7 @@ use rand_distr::{Normal, Distribution};
 // use rand::thread_rng;
 // use rand::prelude::*
 
-fn model(mut rng: Pcg64Mcg) -> f64 {
+fn model(mut rng: &mut Pcg64Mcg) -> f64 {
     let x = 1.0;
     let normal = Normal::new(2.0, 3.0).unwrap();
     let v = normal.sample(&mut rng);
@@ -17,7 +17,7 @@ fn main() {
 
     let mut rng = Pcg64Mcg::seed_from_u64(1);
     // mean 2, standard deviation 3
-    let v = model(rng);
+    let v = model(&mut rng);
     println!("{} is from a N(2, 9) distribution", v)
 }
 
