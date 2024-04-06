3fa55de18f1e7636e3a175f60de9068efc3c09d4
NunoSempere <nuno.semperelh@protonmail.com>
2024-04-06 11:26:23 -0400

    savepoint

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,7 +5,7 @@ use rand_distr::{Normal, Distribution};
 // use rand::thread_rng;
 // use rand::prelude::*
 
-fn model(rng: &mut Pcg64Mcg) -> f64 {
+fn model(rng: Pcg64Mcg) -> f64 {
     let x = 1.0;
     let normal = Normal::new(2.0, 3.0).unwrap();
     let v = normal.sample(&mut rng);
@@ -17,9 +17,9 @@ fn main() {
 
     let mut rng = Pcg64Mcg::seed_from_u64(1);
     // mean 2, standard deviation 3
-    let v = model(&mut rng);
+    let v = model(rng);
     println!("{} is from a N(2, 9) distribution", v);
-    let z = model(&mut rng);
+    let z = model(rng);
     println!("{} is from a N(2, 9) distribution", z);
 }
 
