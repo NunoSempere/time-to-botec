1365d4f21db4a1a45495ab4ca999ffe0011cad05
NunoSempere <nuno.semperelh@protonmail.com>
2024-04-06 00:24:48 -0400

    tweak mutable references (not working tho)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,26 @@
 use rand_core::SeedableRng;
-use rand_distr::{Distribution, Normal};
 use rand_pcg::Pcg64Mcg;
+use rand_distr::{Normal, Distribution};
 
 // use rand::thread_rng;
 // use rand::prelude::*
+
+fn model(rng: &mut Pcg64Mcg) -> f64 {
+    let x = 1.0;
+    let normal = Normal::new(2.0, 3.0).unwrap();
+    let v = normal.sample(&mut rng);
+    v * x
+}
 
 fn main() {
     println!("Hello, world!");
 
     let mut rng = Pcg64Mcg::seed_from_u64(1);
     // mean 2, standard deviation 3
-    let normal = Normal::new(2.0, 3.0).unwrap();
-    let v = normal.sample(&mut rng);
-    println!("{} is from a N(2, 9) distribution", v)
+    let v = model(&mut rng);
+    println!("{} is from a N(2, 9) distribution", v);
+    let z = model(&mut rng);
+    println!("{} is from a N(2, 9) distribution", z);
 }
+
+
